52a715f43f3f2686c35cb359d3690e256ba54140
Ashwin Maroli <ashmaroli@gmail.com>
2017-01-24 10:12:45 +0530

    fix test plugins

--- test/fixtures/another-test-plugin/lib/another-test-plugin.rb
+++ test/fixtures/another-test-plugin/lib/another-test-plugin.rb
@@ -1,7 +1,17 @@
 require "jekyll"
 
-Jekyll::Hooks.register :site, :after_init do |site|
-  file = File.expand_path("sitemap.xml", site.config["source"])
-  File.open(file, "w")
-  File.write(file, "---\n---\n")
+# code abstracted from the official 'jekyll-feed' plugin
+# at https://github.com/jekyll/jekyll-feed/
+
+Jekyll::Hooks.register :site, :pre_render do |site|
+  page = AnotherTestPlugin::NoFile.new(site, "", "", "test-sitemap.xml")
+  site.pages << page
+end
+
+module AnotherTestPlugin
+  class NoFile < Jekyll::Page
+    def read_yaml(*)
+      @data ||= {}
+    end
+  end
 end
