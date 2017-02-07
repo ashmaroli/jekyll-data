9428bf9727a4b900c7e3f6d9ffef342e3023b118
Ashwin Maroli <ashmaroli@gmail.com>
2017-02-07 17:27:21 +0530

    override 'add_build_options' using 'alias_method'

--- lib/jekyll/build_options.rb
+++ lib/jekyll/build_options.rb
@@ -1,29 +1,17 @@
 module Jekyll
   class Command
-    #
-    # patch original method to inject a --data switch to display merged data hash
-    def self.add_build_options(c)
-      c.option "config", "--config CONFIG_FILE[,CONFIG_FILE2,...]",
-        Array, "Custom configuration file"
-      c.option "destination", "-d", "--destination DESTINATION",
-        "The current folder will be generated into DESTINATION"
-      c.option "source", "-s", "--source SOURCE", "Custom source directory"
-      c.option "future", "--future", "Publishes posts with a future date"
-      c.option "limit_posts", "--limit_posts MAX_POSTS", Integer,
-        "Limits the number of posts to parse and publish"
-      c.option "watch", "-w", "--[no-]watch", "Watch for changes and rebuild"
-      c.option "baseurl", "-b", "--baseurl URL",
-        "Serve the website from the given base URL"
-      c.option "force_polling", "--force_polling", "Force watch to use polling"
-      c.option "lsi", "--lsi", "Use LSI for improved related posts"
-      c.option "show_drafts", "-D", "--drafts", "Render posts in the _drafts folder"
-      c.option "unpublished", "--unpublished",
-        "Render posts that were marked as unpublished"
-      c.option "quiet", "-q", "--quiet", "Silence output."
-      c.option "verbose", "-V", "--verbose", "Print verbose output."
-      c.option "show-data", "--show-data",
-        "Print merged site-data hash when used with --verbose."
-      c.option "incremental", "-I", "--incremental", "Enable incremental rebuild."
+    class << self
+      #
+      # patch original method to inject a '--show-data' switch to display
+      # merged data hash
+      #
+      alias_method :original_build_options, :add_build_options
+
+      def add_build_options(c)
+        original_build_options(c)
+        c.option "show-data", "--show-data",
+          "Print merged site-data hash when used with --verbose."
+      end
     end
   end
 end
